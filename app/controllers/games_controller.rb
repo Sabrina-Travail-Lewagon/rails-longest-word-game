fd53293b7f7c7dff8b4c29e166791c2aef71418f
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-05-12 21:05:35 +0200

    Ajout du score et de la session

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,4 @@
 require 'open-uri'
-require 'json'
 
 class GamesController < ApplicationController
   def new
@@ -8,11 +7,19 @@ class GamesController < ApplicationController
   end
 
   def score
+    # On initialise le score :
+    ### Soit le joueur a dejà commencé, du coup, le score aura la valeur de la session
+    ### Soit il débute la partie, le score = 0
+    @score = session[:score] || 0
     @word = params[:word]
     response = URI.open("https://wagon-dictionary.herokuapp.com/#{@word}")
     json = JSON.parse(response.read)
     if json['found'] == true
       @reponse = "Congratulation! #{@word} is a valid English word"
+      # 1 - On incrémente le score avec la valeur du nombre des mots que l'user à saisi
+      @score += @word.length
+      # 2 - On stocke dans la session le score
+      session[:score] = @score
     else
       @reponse = "Sorry but #{@word} doesn't seem to be a valid English word..."
     end
